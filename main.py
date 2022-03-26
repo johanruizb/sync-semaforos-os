af8b181c98111d8bfe2f31633618718d79fdfb35
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2022-03-26 00:02:38 -0500

    corregir solucion

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import threading
 import os
 
-tenedores = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
-tenedoresNumero = [0, 0, 0, 0, 0]
+# Estado neutro
+# tenedores = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
+# tenedoresNumero = [0, 0, 0, 0, 0]
+
+# Estado de inicio
+tenedores = [[0, 0], [0, 0], [0, 0], [0, 1], [1, 0]]
+tenedoresNumero = [0, 1, 1, 1, 1]
 numeroCenas = [0, 0, 0, 0, 0]
-hiloSiguiente = 0
-hilos = [0, 0, 0, 0, 0]
-continua = True
 
 
 def clear_console():
@@ -32,11 +34,13 @@ def visualizar():
 # tenedores[i][1] = Tenedor derecho
 def tenedorIzq(num):
     if num == 0 and tenedores[4][1] - 1 == 0:
+        # print(f'Izq: {num}')
         tenedores[num][0] -= 1
         tenedores[4][1] -= 1
         tenedoresNumero[num] += 1
 
-    elif tenedores[num - 1][1] - 1 == 0:
+    elif num != 0 and tenedores[num - 1][1] - 1 == 0:
+        # print(f'Izq: {num}')
         tenedores[num][0] -= 1
         tenedores[num - 1][1] -= 1
         tenedoresNumero[num] += 1
@@ -44,11 +48,13 @@ def tenedorIzq(num):
 
 def tenedorDer(num):
     if num == 4 and tenedores[0][0] - 1 == 0:
+        # print(f'Der: {num}')
         tenedores[num][1] -= 1
         tenedores[0][0] -= 1
         tenedoresNumero[num] += 1
 
     elif num != 4 and tenedores[num + 1][0] - 1 == 0:
+        # print(f'Der: {num}')
         tenedores[num][1] -= 1
         tenedores[num + 1][0] -= 1
         tenedoresNumero[num] += 1
@@ -77,70 +83,49 @@ def soltarTenedores(num):
         tenedoresNumero[num] -= 1
 
 
-def esperarSiguiente():
-    global continua
-    siguiente = input("¿Desea continuar?")
-    print(siguiente)
-    if str(siguiente) == 'n':
-        continua = False
-    else:
-        clear_console()
-
-
-def continuar():
-    global tenedores
-    global tenedoresNumero
-    global hiloSiguiente
-    global continua
-    hilo_actual = int(threading.current_thread().getName())
-
-    while continua:
-        bloquea.acquire()
-        if hilo_actual == hiloSiguiente:
-            esperarSiguiente()
-
-            tenedorIzq(hilo_actual)
-            tenedorDer(hilo_actual)
-            print(f'= Filosofo {hilo_actual} =')
-            visualizar()
-
-            if tenedoresNumero[hilo_actual] == 2:
-                soltarTenedores(hilo_actual)
-
-            if hiloSiguiente < 4:
-                hiloSiguiente += 1
-            else:
-                hiloSiguiente = 0
-        bloquea.release()
-
-
-def iniciar():
+def continuar(semf):
     global tenedores
     global tenedoresNumero
     hilo_actual = int(threading.current_thread().getName())
 
-    bloquea.acquire()
-    if hilo_actual < 4:
-        if hilo_actual == 0:
-            tenedorDer(4)
+    with semf:
+        tenedorDer(hilo_actual)
         tenedorIzq(hilo_actual)
-    elif hilo_actual == 4:
-        visualizar()
-    bloquea.release()
+
+        if tenedoresNumero[hilo_actual] == 2:
+            numeroCenas[hilo_actual] += 1
+            soltarTenedores(hilo_actual)
+
+
+"""def iniciar(semf):
+    global tenedores
+    global tenedoresNumero
+    hilo_actual = int(threading.current_thread().getName())
+    with semf:
+        if hilo_actual < 4:
+            if hilo_actual == 0:
+                tenedorDer(4)
+            tenedorIzq(hilo_actual)
 
 
 bloquea = threading.Lock()
 
+semaforo = threading.Semaphore(1)
 for num_hilo in range(5):
-    hilos[num_hilo] = threading.Thread(name=f'{num_hilo}', target=iniciar)
-    hilos[num_hilo].start()
-
-for hilo in hilos:
+    hilo = threading.Thread(name=f'{num_hilo}', target=iniciar, args=(semaforo,))
+    hilo.start()
     hilo.join()
+"""
+visualizar()
 
-for num_hilo in range(5):
-    hilos[num_hilo] = threading.Thread(name=f'{num_hilo}', target=continuar)
-    hilos[num_hilo].start()
+number = input('Numero de iteraciones deseadas: ')
+semaforo = threading.Semaphore(5)
+for iteraciones in range(int(number)):
+    for num_hilo in range(5):
+        hilo = threading.Thread(name=f'{num_hilo}', target=continuar, args=(semaforo,))
+        hilo.start()
+        hilo.join()
 
-for hilo in hilos:
-    hilo.join()
+clear_console()
+print(f'= Resultado despues de {number} iteraciones =')
+visualizar()
